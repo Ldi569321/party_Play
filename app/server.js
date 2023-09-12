f2a43376dc4a017e749a8e5a62a1882fdd382b5f
Ldi569321 <81796049+Ldi569321@users.noreply.github.com>
2023-09-12 15:19:03 +0900

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,10 +1,5 @@
 const mysql = require('mysql');  // mysql 모듈 로드
 const conn = mysql.createConnection({  // mysql 접속 설정
-  host: 'partyplay.cqysxu3bw1nu.ap-northeast-2.rds.amazonaws.com',
-  port: '3306',
-  user: 'root',
-  password: 'ldi569321',
-  database: 'partyPlay'
 });
 
 const express = require('express');
